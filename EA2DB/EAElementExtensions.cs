ec7c1db1bd5c3be5522eb0b6d35f303d49b21f46
Rondomat <Rondomat@unknown.net>
2012-08-09 15:32:51 +0200

    Firrst code comitted

---
+++ EA2DB/EAElementExtensions.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EAElementExtensions.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace EA2DB
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Data;
+	using System.Data.OleDb;
+
+	/// <summary>
+	/// TODO: Update summary.
+	/// </summary>
+	public static class EAElementExtensions
+	{
+		#region ### Extension Methods ###
+
+
+		#endregion ### Extension Methods ###
+
+		public static DataSet OperationsOfClassLike(this IEnumerable<string> names, bool bLike, OleDbConnection connection)
+		{
+			DataSet dataSet = new DataSet();
+			
+			foreach(var name in names)
+			{
+				var childDataSet = OperationsOfClassLike(name, bLike, connection);
+				dataSet.Merge(childDataSet.Tables[0]);
+			}
+			return dataSet;
+		}
+
+		public static DataSet OperationsOfClassLike(this string name, bool bLike, OleDbConnection connection)
+		{
+			DataSet dataSet = new DataSet();
+			string like = string.Format(OPERATIONS_OF_CLASS_LIKE, name);
+			string query = string.Format("{0}{1}", OPERATIONS_OF_CLASS_SELECT, like);
+			OleDbCommand command = new OleDbCommand(query, connection);
+			OleDbDataAdapter a = new OleDbDataAdapter(command);
+			a.Fill(dataSet);
+			DataColumn[] primaryKeys = new DataColumn[1];
+			primaryKeys[0] = dataSet.Tables[0].Columns[0];
+			dataSet.Tables[0].PrimaryKey = primaryKeys;
+			return dataSet;
+		}
+
+
+
+		private const string OPERATIONS_OF_CLASS_SELECT = @"
+SELECT t_operation.OperationID AS Operation_ID,
+t_operation.Name AS Operation_Name,
+t_object.Object_ID,
+t_object.Name AS Object_Name
+FROM (t_operation INNER JOIN t_object ON t_operation.Object_ID = t_object.Object_ID)";
+		private const string OPERATIONS_OF_CLASS_WHERE = @"WHERE (t_object.Object_ID = {0})";
+		private const string OPERATIONS_OF_CLASS_LIKE = @"WHERE (t_object.Name LIKE '%{0}%')";
+		private const string OPERATIONS_OF_CLASS_EXACT = @"WHERE (t_object.Name = {0})";
+	}
+}
---
+++ EA2DB/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Data;
+/*
+ * Created by SharpDevelop.
+ * User: Ron
+ * Date: 07.08.2012
+ * Time: 20:49
+ *
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace EA2DB
+{
+	using System;
+	using System.Data;
+	using System.Data.OleDb;
+	using System.Linq;
+	using System.Collections.Generic;
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+
+				string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\temp\test.EAP";
+				using(OleDbConnection connection = new OleDbConnection(connectionString))
+				{
+					connection.Open();
+
+					List<string> opNames = new List<string>();
+
+					opNames.Add("Version");
+					opNames.Add("main");
+					opNames.Add("main");
+					opNames.Add("ma");
+
+					DataSet dataSet = opNames.OperationsOfClassLike(true, connection);
+					//DataSet dataSet = "un".OperationsOfClassLike(true, connection);
+					DumpDS(dataSet);
+
+					var names = dataSet.Tables[0].AsEnumerable().Select(x => new { X = x.Field<string>(1), Y = x.Field<string>(3) });
+					names.ToList().ForEach(x =>
+					{ 
+						Console.WriteLine("Class: {0}", x.X);
+						Console.WriteLine("Operation: {0}", x.Y);
+					});
+
+
+
+				}
+			}
+			catch(System.Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			//Console.Write("Press any key to continue . . . ");
+			//Console.ReadKey(true);
+		}
+
+		private static void Dump(OleDbCommand command)
+		{
+			OleDbDataReader reader = command.ExecuteReader();
+			while(reader.Read())
+			{
+				for(int fieldCount = 0; fieldCount < reader.FieldCount; fieldCount++)
+				{
+					Console.WriteLine(string.Format("[{0}] {1} ({2}):{3}",
+													fieldCount,
+													reader.GetName(fieldCount),
+													reader.GetDataTypeName(fieldCount),
+													reader[fieldCount].ToString()));
+				}
+			}
+			reader.Close();
+		}
+		private static void DumpDS(OleDbCommand command)
+		{
+			DataSet dataSet = new DataSet();
+			OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+			adapter.Fill(dataSet);
+			DumpDS(dataSet);
+		}
+
+		private static void DumpDS(DataSet dataSet)
+		{
+			foreach(DataTable table in dataSet.Tables)
+			{
+				Console.WriteLine("++++{0}+++++", table.TableName);
+				Console.Write("Column-> |");
+				int count = 0;
+				foreach(DataColumn column in table.Columns)
+				{
+					Console.Write("{0}:{1} [{2}]|", count++, column.ColumnName, column.DataType);
+				}
+				Console.WriteLine("-----------");
+				count = 0;
+				foreach(DataRow row in table.Rows)
+				{
+					Console.Write("{0}-> |", count++);
+					int innerCount = 0;
+					foreach(var item in row.ItemArray)
+					{
+						Console.Write("{0}:{1}|", innerCount++, item);
+					}
+				}
+				Console.WriteLine("+++++++++");
+			}
+		}
+	}
+}
